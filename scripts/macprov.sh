209c2dd9b82b999a8b1bcf00204698471d04b471
Daniel Clark <daniel.clark@liveramp.com>
2017-01-27 19:11:55 -0800

    Added more functionality.

--- scripts/macprov.sh
+++ scripts/macprov.sh
@@ -21,12 +21,27 @@ parse_args() {
         usage
         exit
         ;;
+      -w|--workspace)
+        if [ -n "$2" ]; then
+          WORKSPACE=$2
+          shift
+          shift
+        else
+          echo -e "ERROR: '--workspace' requires a non-empty string argument" >&2
+          usage
+          exit 1
+        fi
+        ;;
     esac
   done
 }
 
 main() {
+
+  parse_args "$@"
+
   # Install homebrew
+  echo -e "Installing homebrew...\n"
   /usr/bin/ruby -e \
       "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -35,4 +50,14 @@ main() {
 
   # Install packages
   brew install htop watch
+
+  if [ -z "$WORKSPACE" ]; then
+    echo -e "\nWorkspace not set, auto-setting to $HOME/Projects...\n"
+    WORKSPACE="$HOME"/Projects
+    mkdir -p "$WORKSPACE"
+  else
+    echo -e "\nUsing $WORKSPACE for workspace...\n"
+  fi
 }
+
+main "$@"
