7173739c2dfd3e81bcd3c7aa0ccd454925371fb2
dclark87 <danieljclark87@gmail.com>
2017-01-29 23:40:54 -0500

    Updated setup script for mac.

--- scripts/macprov.sh
+++ scripts/macprov.sh
@@ -9,8 +9,7 @@ usage: $(basename "$0") <args>
   $(basename "$0") provisions a Mac with a functional development environment.
 
 args:
-  -w [workspace]  Path to base working directory directory.
-
+  --workspace, -w <workspace>  Path to base working directory directory.
 EOF
 }
 
@@ -32,6 +31,15 @@ parse_args() {
           exit 1
         fi
         ;;
+      *)
+        if [ -n "$WORKSPACE" ]; then
+          break
+        else
+          echo -e "ERROR: specify '--workspace' arg" >&2
+          usage
+          exit 1
+        fi
+        ;;
     esac
   done
 }
@@ -40,17 +48,26 @@ main() {
 
   parse_args "$@"
 
-  # Install homebrew
-  echo -e "Installing homebrew...\n"
-  /usr/bin/ruby -e \
-      "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  # Install homebrew.
+  BREW=$(which brew)
+  if [ -z "$BREW" ]; then
+      echo -e "Installing homebrew...\n"
+      /usr/bin/ruby -e \
+          "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
 
-  # Install brew cask
+  # Install brew cask.
   brew tap caskroom/cask
 
-  # Install packages
-  brew install htop watch
+  # Install packages via brew.
+  brew install go htop watch
 
+  # Git completion setup.
+  mkdir -p "$HOME"/.git
+  curl -LSso "$HOME"/.git/git-completion.bash \
+      https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+
+  # Check for workspace being set.
   if [ -z "$WORKSPACE" ]; then
     echo -e "\nWorkspace not set, auto-setting to $HOME/Projects...\n"
     WORKSPACE="$HOME"/Projects
@@ -58,6 +75,37 @@ main() {
   else
     echo -e "\nUsing $WORKSPACE for workspace...\n"
   fi
+
+  # Clone projects.
+  DEV_REPO="$WORKSPACE"/dev
+  if [ ! -d "$DEV_REPO" ]; then
+    echo -e "Cloning developer repository...\n"
+    git clone https://github.com/dclark87/dev.git $DEV_REPO
+  fi
+
+  # Update submodules.
+  echo -e "Updating submodules...\n"
+  cd $DEV_REPO
+  git submodule init && git submodule update --recursive
+
+  # Symlink config files.
+  echo -e "Symlinking config files...\n"
+  ln -fs "$DEV_REPO"/dotfiles/bash_profile "$HOME"/.bash_profile
+  ln -fs "$DEV_REPO"/dotfiles/pypirc "$HOME"/.pypirc
+  ln -fs "$DEV_REPO"/dotfiles/tmux.conf "$HOME"/.tmux.conf
+  ln -fs "$DEV_REPO"/dotfiles/vimrc "$HOME"/.vimrc
+  ln -fs "$DEV_REPO"/dotfiles/vim "$HOME"/.vim
+
+  # Source bash_profile
+  source "$HOME"/.bash_profile
+
+  # Install pathogen for vim.
+  echo -e "Installing pathogen for vim...\n"
+  curl -LSso "$HOME"/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+  # Go setup/
+  echo -e "Creating GOPATH: $GOPATH..."
+  mkdir -p "$GOPATH"
 }
 
 main "$@"
